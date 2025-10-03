f010d2dcc7fe66a0557115823fc25a094298643e
Bizcocho <78247044+mandradegr@users.noreply.github.com>
2025-10-03 12:08:58 -0400

    First upload 70 files, static website
    I am not using export ,

---
+++ _next/static/chunks/app/research/page-84b642ccb5ac7d57.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[246,322,974],{5707:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,926,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[926,476,571,358],()=>s(5707)),_N_E=e.O()}]);
